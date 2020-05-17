d333f59ff4a366d6a6b1742907cf6122f12306b0
Cassandra Dew <cpdew2@gmail.com>
2020-05-17 19:53:51 -0400

    start App file setting up nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Form from "./Form";
+import Home from "./Home";
 
-const App = () => {
+
+
+function App () {
   return (
-    <>
+
+    <Router>
+
+      <nav className="navbar">
+        <Link to="/">
+          <button name='homebutton'>Home</button>
+        </Link>
+
+      </nav>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
     </>
