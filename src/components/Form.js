8d4364dc26a2c4c2c362566084f2050a01e8e729
Cassandra Dew <cpdew2@gmail.com>
2020-05-17 20:26:15 -0400

    add useState

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,11 +8,44 @@ const formSchema = yup.object().shape({
     size: yup.string().required("Must selet a size"),
     pepperoni: yup.boolean().defined(),
     beef: yup.boolean().defined(),
+    sausage: yup.boolean().defined(),
     blackolives: yup.boolean().defined(),
     pineapple: yup.boolean().defined(),
     peppers: yup.boolean().defined(),
-    sausage: yup.boolean().defined(),
     extracheese: yup.boolean().defined(),
     specInstructions: yup.string().notRequired()
 });
 
+export default function form() {
+    //adding state
+
+    const [formState, setFormState] = useState({
+        name: "",
+        size: "",
+        pepperoni: false,
+        beef: false,
+        sausage: false,
+        blackolives: false,
+        pineapple: false,
+        peppers: false,
+        extracheese: false,
+        specInstructions: ""
+    })
+
+    // errors
+
+    const [errors, setErrors] = useState({
+        name: "",
+        size: "",
+        pepperoni: "",
+        beef: "",
+        sausage: "",
+        blackolives: "",
+        pineapple: "",
+        peppers: "",
+        extracheese: "",
+        specInstructions: "",
+    })
+
+    //useState for button
+}
