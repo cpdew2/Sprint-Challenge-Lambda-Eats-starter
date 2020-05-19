9a18519b6b52ae08950101916d3e81e215dee840
Cassandra Dew <cpdew2@gmail.com>
2020-05-18 22:25:46 -0400

    MVP

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Form from './Form';
+
 
 function Home() {
     return (
         <div className="home">
             <h1>The Best Pizza Ever</h1>
-            <Form />
+            
           
         </div>
     );
